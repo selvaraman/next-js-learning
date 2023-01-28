956a7425297872af9237a000382fe9857b732570
Selvam Ramsamy <selvam@blockchainappfactory.com>
2023-01-28 14:57:34 +0530

    issue fixing

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,20 +14,20 @@ const Home: NextPage = () => {
     console.log(await provider?.listAccounts());
   }
 console.log(contract)
-
+console.log("Is Loading:", isLoading)
+console.log("ethereum:", ethereum)
   const contractInfo = async () => {
     console.log(await contract!.name())
     console.log(await contract!.symbol())
   }
   if(contract) {
-    contractInfo();
+   contractInfo();
   }
   if(provider) {
     getAccounts();
   }
   return (
     <BaseLayout>
-    {`isLoading: ${ethereum}`}
       <div className="relative bg-gray-50 pt-16 pb-20 px-4 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8">
         <div className="absolute inset-0">
           <div className="bg-white h-1/3 sm:h-2/3" />
